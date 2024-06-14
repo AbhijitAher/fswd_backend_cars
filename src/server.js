879d5b1303fe243ffa2a25c377f94b8e148dad0b
Abhijit Aher <seabhi.aher@gmail.com>
2024-06-14 17:01:52 +0530

    inventory api initiated

--- src/server.js
+++ src/server.js
@@ -16,3 +16,20 @@ app.use(cors());
 app.listen(process.env.PORT, () => {
   console.log("Car API backend server Running on port: " + process.env.PORT);
 });
+
+const getCarInventoryWithFilters = async (req, res) => {
+  try {
+    const allInventoryCars = {}
+
+    let obj = {
+      data: allInventoryCars,
+    };
+
+    res.status(200).send(obj);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Error in getting car inventory");
+  }
+};
+
+app.get("/api/inventory", getCarInventoryWithFilters);
